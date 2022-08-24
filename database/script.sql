a80721fc6803a964d7db251bb85a85d2a83cc647
ptanh2k <pta230500x090800@gmail.com>
2022-08-24 13:31:09 +0700

    Modified to script.sql

--- database/script.sql
+++ database/script.sql
@@ -1,8 +1,14 @@
-CREATE DATABASE cycir;
+CREATE DATABASE cycir WITH ENCODING 'UTF8' TEMPLATE template0;
 
 CREATE EXTENSION IF NOT EXISTS ltree;
 
-CREATE TABLE groups_ (
-	name VARCHAR(30) NOT NULL,
-	path ltree UNIQUE NOT NULL PRIMARY KEY
+CREATE TABLE group_ (
+	name VARCHAR(50) NOT NULL PRIMARY KEY,
+	name_in_path VARCHAR(60) NOT NULL,
+	path ltree UNIQUE NOT NULL
 )
+
+INSERT INTO group_ (name, name_in_path, path) VALUES
+('Viettel Ticket', 'Viettel_Ticket', 'Viettel_Ticket'),
+('Tier 1', 'Tier_1', 'Viettel_Ticket.Tier_1'),
+('Tier 2', 'Tier_2', 'Viettel_Ticket.Tier_2')
