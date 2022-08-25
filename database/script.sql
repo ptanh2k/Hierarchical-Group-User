7c123c7fe01ce01673f0d5fbbaebc9dd84190441
ptanh2k <pta230500x090800@gmail.com>
2022-08-25 09:52:05 +0700

    Add sample

--- database/script.sql
+++ database/script.sql
@@ -11,4 +11,23 @@ CREATE TABLE group_ (
 INSERT INTO group_ (name, name_in_path, path) VALUES
 ('Viettel Ticket', 'Viettel_Ticket', 'Viettel_Ticket'),
 ('Tier 1', 'Tier_1', 'Viettel_Ticket.Tier_1'),
-('Tier 2', 'Tier_2', 'Viettel_Ticket.Tier_2')
+('Tier 2', 'Tier_2', 'Viettel_Ticket.Tier_2'),
+
+INSERT INTO group_ (name, name_in_path, path) VALUES
+('Group Viettel 1', 'Group_Viettel_1', 'Viettel_Ticket.Tier_1.Group_Viettel_1'),
+('Group Viettel 2', 'Group_Viettel_2', 'Viettel_Ticket.Tier_1.Group_Viettel_2'),
+('Group Viettel 3', 'Group_Viettel_3', 'Viettel_Ticket.Tier_1.Group_Viettel_3')
+
+INSERT INTO group_ (name, name_in_path, path) VALUES
+('Group Viettel 1', 'Group_Viettel_1', 'Viettel_Ticket.Tier_2.Group_Viettel_1'),
+('Group Viettel 2', 'Group_Viettel_2', 'Viettel_Ticket.Tier_2.Group_Viettel_2'),
+('Group Viettel 3', 'Group_Viettel_3', 'Viettel_Ticket.Tier_2.Group_Viettel_3')
+
+SELECT * FROM group_
+
+SELECT * FROM group_ WHERE path @ 'Tier_1'
+
+-- Change PRIMARY KEY for group_ table --
+ALTER TABLE group_ DROP CONSTRAINT group__pkey;
+
+ALTER TABLE group_ ADD PRIMARY KEY (path);
